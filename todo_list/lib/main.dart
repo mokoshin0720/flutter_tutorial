54bbf12d9bd0c4f823e7fbc5eac18e8041fcb9b3
Shinya Kinjo <65533712+mokoshin0720@users.noreply.github.com>
2022-04-10 12:54:38 +0900

    [fix]divide screen folder

--- todo_list/lib/main.dart
+++ todo_list/lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:todo_list/screen/list.dart';
 
 void main() {
   // 最初に表示するWidget
@@ -17,104 +16,6 @@ class MyTodoApp extends StatelessWidget {
       ),
       // リスト一覧画面を表示
       home: TodoListPage(),
-    );
-  }
-}
-
-class TodoListPage extends StatefulWidget {
-  @override
-  _TodoListPageState createState() => _TodoListPageState();
-}
-
-// リスト一覧画面用Widget
-class _TodoListPageState extends State<TodoListPage> {
-  List<String> todoList = [];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('リスト一覧'),
-      ),
-      body: ListView.builder(
-        itemCount: todoList.length,
-        itemBuilder: (context, index) {
-          return Card(
-            child: ListTile(
-              title: Text(todoList[index]),
-            ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          final newListText = await Navigator.of(context)
-              .push(MaterialPageRoute(builder: (context) {
-            return TodoAddPage();
-          }));
-          if (newListText != null) {
-            setState(() {
-              todoList.add(newListText);
-            });
-          }
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class TodoAddPage extends StatefulWidget {
-  @override
-  _TodoAddPageState createState() => _TodoAddPageState();
-}
-
-class _TodoAddPageState extends State<TodoAddPage> {
-  String _text = '';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('リスト追加'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(64),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(_text, style: TextStyle(color: Colors.blue)),
-            const SizedBox(height: 8),
-            TextField(
-              onChanged: (String value) {
-                setState(() {
-                  _text = value;
-                });
-              },
-            ),
-            const SizedBox(height: 8),
-            Container(
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(_text);
-                },
-                child: Text('リスト追加', style: TextStyle(color: Colors.blue)),
-              ),
-            ),
-            const SizedBox(height: 8),
-            Container(
-              width: double.infinity,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: Text('キャンセル'),
-              ),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ todo_list/lib/screen/list.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/screen/add.dart';
+
+class TodoListPage extends StatefulWidget {
+  @override
+  _TodoListPageState createState() => _TodoListPageState();
+}
+
+// リスト一覧画面用Widget
+class _TodoListPageState extends State<TodoListPage> {
+  List<String> todoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('リスト一覧'),
+      ),
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              title: Text(todoList[index]),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final newListText = await Navigator.of(context)
+              .push(MaterialPageRoute(builder: (context) {
+            return TodoAddPage();
+          }));
+          if (newListText != null) {
+            setState(() {
+              todoList.add(newListText);
+            });
+          }
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
